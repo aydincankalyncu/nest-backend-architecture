df99e96e3e463d8040f33e9c89fd3a2a4baa94d9
aydincankalyncu <aydincankalyncu@gmail.com>
2024-03-06 21:51:41 +0300

    Yaml configurations implemented for config service.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService){}
   getHello(): string {
+    const test = this.configService.get<string>("db.mongo.url");
+    console.log(test);
     return 'Hello World!';
   }
 }
