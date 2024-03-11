7424d1ee3da5026a3c31316eecc320eca10e02a7
aydincankalyncu <aydincankalyncu@gmail.com>
2024-03-11 16:29:51 +0300

    Swagger document added.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest Framework Architecture API')
+    .setDescription('The Nest Framework API')
+    .setVersion('1.0')
+    .addTag('arch')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
